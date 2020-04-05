ab0069bdf7c0e63bd8a3f3b0bee00fb53cf02cf0
vishalsingh384 <41135641+vishalsingh384@users.noreply.github.com>
2020-04-05 14:27:26 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+    // Animate loader off screen
+    setTimeout(
+        function() 
+        {
+            $(".se-pre-con").fadeOut("slow");;
+          //do something special
+        }, 2000);
+    
+});
